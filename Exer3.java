6b083e04d541d4d387aae3e4e0c877724e145a9c
ikatahira <isaque.katahira@fatec.sp.gov.br>
2021-09-08 23:38:47 +0000

    Fors alinhados

---
+++ Exer3.java
@@ -0,0 +1,15 @@
+
+
+public class Exer3{
+    public static void main(String args[]){
+        for(int n=1;n<=4;n++){
+            for(int j=1;j<=n;j++){
+                System.out.print(n*j+ " ");
+
+            }
+            System.out.println();
+
+        }
+    }
+
+}
